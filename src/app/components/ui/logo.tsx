7d47ebb9dd3a83578fa42cc1191085409e582492
sergio-sanchez-pragmaticdlt <sergio.sanchez@pragmaticdlt.com>
2024-08-29 17:06:26 -0300

    add name to link home

--- src/app/components/ui/logo.tsx
+++ src/app/components/ui/logo.tsx
@@ -3,8 +3,9 @@ import LogoIcon from "src/app/assets/icons/logo-icon";
 
 export default function Logo() {
   return (
-    <Link className="block" href="/">
+    <Link className="block relative" href="/">
       <LogoIcon className="size-14" />
+      <span className="absolute text-xs text-transparent">home</span>
     </Link>
   );
 }
